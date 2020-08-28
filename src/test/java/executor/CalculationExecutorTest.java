f6afa9c41ea99cb9967f5414df4039978af2e57f
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-08-28 09:45:00 +0200

    Initial commit

---
+++ src/test/java/executor/CalculationExecutorTest.java
@@ -0,0 +1,49 @@
+package executor;
+
+import calculator.Calculator;
+import command.MultiplyCommand;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculationExecutorTest {
+
+    @Test
+    void shouldReturnCorrectResult() {
+        //given
+        CalculationExecutor calculationExecutor = new CalculationExecutor(new Calculator());
+
+        //when
+        String operation = "apply 10";
+        String[] commandArr = operation.split(" ");
+
+        //then
+        assertEquals(10, calculationExecutor.analyzeOperation(operation));
+    }
+
+    @Test
+    void shouldExecuteCorrectOperation() {
+        //given
+        Calculator calculator = new Calculator();
+
+        //when
+        int currentNumber = 2;
+        String operation = "multiply 2";
+        String[] commandArr = operation.split(" ");
+        MultiplyCommand command = new MultiplyCommand(currentNumber, Integer.parseInt(commandArr[1]));
+
+        //then
+        assertEquals(4, calculator.calculate(command));
+    }
+
+    @Test
+    void shouldThrowException() {
+        CalculationExecutor calculationExecutor = new CalculationExecutor(new Calculator());
+
+        int currentNumber = 1;
+        String operation = "";
+        String[] commandArr = operation.split(" ");
+
+        assertThrows(IllegalArgumentException.class, () -> calculationExecutor.executeOperation(currentNumber, operation));
+    }
+}
